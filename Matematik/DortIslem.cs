0255cf2a84e27d2ada218b92f801606f57fd0f81
Besim <Besim@DESKTOP-4O15TK1>
2021-01-20 23:50:52 +0300

    jjjj

---
+++ Matematik/DortIslem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematik
+{
+    class DortIslem
+    {
+        //cagırırken Topla(2,3) sayı1=2 sayı2=3
+        public void Topla(int sayi1,int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine("Sonuc : " + toplam);
+        }
+    }
+}
---
+++ metotlar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+
+            //type safe -- çalışacağı veri tipini bilmek ister c#
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------------------");
+
+            }
+
+            Console.WriteLine("----------------Metotlar---------------");
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            //sepetManager.Ekle2("Armut","Yeşil Armut", 12);
+
+        }
+    }
+}
